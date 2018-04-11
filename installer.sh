88937f83418d9228b0464526a38c78cef02cd1db
marcos <jkmarcosb@gmail.com>
2018-04-11 16:52:39 -0600

    adding sublimte and oh my zsh

--- installer.sh
+++ installer.sh
@@ -8,10 +8,16 @@ if [ "$PACKAGE" = "apt"]; then
 fi
 
 sudo $PACKAGE $INSTALL_COMMAND vim
-sudo $PACKAGE $INSTALL_COMMAND keepassx
 sudo $PACKAGE $INSTALL_COMMAND zsh
 sudo $PACKAGE $INSTALL_COMMAND meld
 sudo $PACKAGE $INSTALL_COMMAND curl
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo ""
 echo "GNU/Linux rocks !"
