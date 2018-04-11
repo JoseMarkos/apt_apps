b5aed9c7c84c008809537a36ee84fe0f6f8e5972
marcos <jkmarcosb@gmail.com>
2018-04-11 17:07:46 -0600

    adding ssh keygen

--- installer.sh
+++ installer.sh
@@ -16,8 +16,10 @@ sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+ssh-keygen
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 
 echo ""
 echo "GNU/Linux rocks !"
