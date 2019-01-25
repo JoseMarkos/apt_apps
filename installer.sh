f07e2a203efe1903c897ea395e254803691f9574
jmarkos <jkmarcosb@gmail.com>
2019-01-25 13:10:41 -0600

    one line package manager

--- installer.sh
+++ installer.sh
@@ -7,20 +7,20 @@ if [ "$PACKAGE" = "apt"]; then
 	echo sudo $PACKAGE update
 fi
 
-sudo $PACKAGE $INSTALL_COMMAND vim
-sudo $PACKAGE $INSTALL_COMMAND zsh
-sudo $PACKAGE $INSTALL_COMMAND meld
-sudo $PACKAGE $INSTALL_COMMAND curl
+sudo $PACKAGE $INSTALL_COMMAND vim zsh meld curl
+
+# sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+# ssh 
 ssh-keygen
 
+# oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 
 echo ""
 echo "GNU/Linux rocks !"
-
